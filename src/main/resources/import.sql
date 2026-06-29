cc08a271d10bf4f7a18d473137fb67eb01ab1fca
Bruno Amichi <bruno.amichi@ifood.com.br>
2026-06-29 09:54:12 -0300

    QUARKUS - ADD PANACHE, framework de orm simplificada para quarkus com base no hibernate

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into Pessoa(id, nome, anoNascimento)  values(1, 'Bruno', 1989);
+insert into Pessoa(id, nome, anoNascimento) values(2, 'AMICHI', 1989);
+insert into Pessoa(id, nome, anoNascimento) values(3, 'Maite', 2022);
+insert into Pessoa(id, nome, anoNascimento) values(5, 'TETE', 1988);
+insert into Pessoa(id, nome, anoNascimento) values(6, 'Romeu', 2026);
+insert into Pessoa(id, nome, anoNascimento) values(7, 'DAVI', 2020);
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 7;
